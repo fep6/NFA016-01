e4df3bc22deab75f9d0a9dae6ebdf2fc2adc3f55
fep <franck.en.planck@gmail.com>
2021-05-06 14:17:16 +0200

    mef

--- NFA016-01/WebContent/js/exerciceJS.js
+++ NFA016-01/WebContent/js/exerciceJS.js
@@ -1,6 +1,7 @@
-alert ("hello");
+alert ("Début de script");
+
+$('body').css('background-color', 'orange');
 
 $("h1").html("Nouveau <i>titre</i> <i class='deuz'> en deux parties</i>");
 $("img#borderBlue").attr("src","https://en.freejpg.com.ar/asset/400/b8/b81c/F100023754.jpg");
-$("p#change").html('On en fait des villes!');
-$("p#change").txt('On en fait de très grandes villes!');
+$("p#change").html('...Mais on en fait des villes!');
