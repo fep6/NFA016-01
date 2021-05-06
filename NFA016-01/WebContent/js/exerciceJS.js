adeb75c60a1681459c7274b92949e4d5887799d1
fep <franck.en.planck@gmail.com>
2021-05-06 10:03:56 +0200

    Exercice 5 session 3 exerciceJS

---
+++ NFA016-01/WebContent/js/exerciceJS.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+$("h1").html("Nouveau <i>titre</i> <i class='deuz'> en deux parties</i>");
+$("img#borderBlue").attr("src","https://en.freejpg.com.ar/asset/400/b8/b81c/F100023754.jpg");
+$("p#change").html('On en fait des villes!');
+$("p#change").txt('On en fait de très grandes villes!');
