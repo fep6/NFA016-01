1b3f0a8bdac3af99f94b0f4c190c3fb0617bfa30
fep <franck.en.planck@gmail.com>
2021-05-06 13:44:37 +0200

    Chargement jQuery ok

--- NFA016-01/WebContent/js/exerciceJS.js
+++ NFA016-01/WebContent/js/exerciceJS.js
@@ -1,6 +1,5 @@
-/**
- * 
- */
+alert ("hello");
+
 $("h1").html("Nouveau <i>titre</i> <i class='deuz'> en deux parties</i>");
 $("img#borderBlue").attr("src","https://en.freejpg.com.ar/asset/400/b8/b81c/F100023754.jpg");
 $("p#change").html('On en fait des villes!');
